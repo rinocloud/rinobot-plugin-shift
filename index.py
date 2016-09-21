1977c56e84436975bc18e13a11e84001fab84d3f
eoinmurray <amonecho1@gmail.com>
2016-09-21 16:05:52 +0100

    space

--- index.py
+++ index.py
@@ -11,6 +11,8 @@ def main():
     index = bot.index_from_args(data)
     data[index] = data[index] + shift
 
+
+
     outname = bot.no_extension() + '-shift-%s.txt' % shift
 
     # then we set up the output
